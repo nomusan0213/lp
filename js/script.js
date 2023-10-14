d5aea9360d7f6775d31de6c5b27b3ef753a156d5
nomusan0213 <yuta.inti.raymi0213@icloud.com>
2023-10-14 22:11:11 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -2,95 +2,57 @@
 ==== LOGOの表示
 *************************/
 $(window).on('load',function(){
-    $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-    // $("#logo-element").delay(3000).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+  $("#loading").delay(1500).fadeOut('slow'); // ローディング画面を1.5秒（1500ms）待機してからフェードアウト
 });
-
 
 /*************************
 ==== 最初の画面
 *************************/
 // スクロールイベントを監視
 $(window).scroll(function() {
-  // ページトップからのスクロール量を取得
-  var scrollPosition = $(this).scrollTop();
+// ページトップからのスクロール量を取得
+var scrollPosition = $(this).scrollTop();
 
-  // フェードアウトさせたい要素
-  var logoElement = $("#logo-element");
+// フェードアウトさせたい要素
+var logoElement = $("#logo-element");
 
-  // スクロール位置に応じて透明度を設定
-  // ここではスクロール位置が200px未満の場合は完全に不透明、それ以外はフェードアウト
-  if (scrollPosition < 400) {
-    logoElement.css("opacity", 1.0);
-  } else {
-    logoElement.css("opacity", 0.0);
-  }
-});
-
-/*************************
-==== 楽しい記憶を記録の文字
-*************************/
-// スクロールイベントを監視
-$(window).scroll(function() {
-  var scrollPosition = $(this).scrollTop();
-  var logoElement = $(".text-sec2");
-  var windowWidth = $(window).width(); // ウィンドウの横幅を取得
-
-  if (windowWidth <= 425) { // 横幅が425以下の場合
-    if (scrollPosition < 400) {
-      logoElement.css("opacity", 0);
-    } else if (scrollPosition >= 400 && scrollPosition < 450) {
-      logoElement.css("opacity", 0.5);
-    } else if (scrollPosition >= 450 && scrollPosition < 600) {
-      logoElement.css("opacity", 1);
-    } else if (scrollPosition >= 600 && scrollPosition < 650) {
-      logoElement.css("opacity", 0.5);
-    } else {
-      logoElement.css("opacity", 0);
-    }
-  } else { // 横幅が425より大きい場合
-    if (scrollPosition < 500) {
-      logoElement.css("opacity", 0);
-    } else if (scrollPosition >= 600 && scrollPosition < 800) {
-      logoElement.css("opacity", 0.5);
-    } else if (scrollPosition >= 800 && scrollPosition < 1200) {
-      logoElement.css("opacity", 1);
-    } else if (scrollPosition >= 1200 && scrollPosition < 1500) {
-      logoElement.css("opacity", 0.5);
-    } else {
-      logoElement.css("opacity", 0);
-    }
-  }
+// スクロール位置に応じて透明度を設定
+// ここではスクロール位置が200px未満の場合は完全に不透明、それ以外はフェードアウト
+if (scrollPosition < 400) {
+  logoElement.css("opacity", 1.0);
+} else {
+  logoElement.css("opacity", 0.0);
+}
 });
 
 /*************************
 ==== TOP画面スライドショー
 *************************/
 $(document).ready(function() {
-  let images; // 画像要素を格納する変数
+let images; // 画像要素を格納する変数
 
-  // 画面幅に応じて異なるクラスを持つ要素を選択
-  if ($(window).width() >= 768) {
-    images = $(".header-img"); // 横幅が768以上の場合
-  } else {
-    images = $(".header-img-2"); // 横幅が768未満の場合
-  }
+// 画面幅に応じて異なるクラスを持つ要素を選択
+if ($(window).width() >= 768) {
+  images = $(".header-img"); // 横幅が768以上の場合
+} else {
+  images = $(".header-img-2"); // 横幅が768未満の場合
+}
 
-  let currentIndex = 0; // 現在の画像インデックス
+let currentIndex = 0; // 現在の画像インデックス
 
-  // 画像切り替え関数
-  function changeImage() {
-    images.hide(); // すべての画像を非表示に
-    images.eq(currentIndex).show(); // 現在の画像を表示
-    currentIndex = (currentIndex + 1) % images.length; // 次の画像インデックスを計算
-  }
+// 画像切り替え関数
+function changeImage() {
+  images.hide(); // すべての画像を非表示に
+  images.eq(currentIndex).show(); // 現在の画像を表示
+  currentIndex = (currentIndex + 1) % images.length; // 次の画像インデックスを計算
+}
 
-  // 初期状態として最初の画像を表示
-  images.eq(currentIndex).show();
+// 初期状態として最初の画像を表示
+images.eq(currentIndex).show();
 
-  // 2秒ごとに画像を切り替える
-  setInterval(changeImage, 4000);
-});
+// 2秒ごとに画像を切り替える
+setInterval(changeImage, 4000);
+})
 
 /*************************
 ==== section2の画像の透明度を調節
@@ -208,6 +170,7 @@ $(document).ready(function() {
 ==== フッター
 *************************/
 $(document).ready(function() {
+  var logoElement = $(".text-sec2");
   $(window).scroll(function() {
     // スクロール位置を取得
     var scrollPosition = $(window).scrollTop();
@@ -222,6 +185,7 @@ $(document).ready(function() {
 
     // フッターのテキストを初期化
     $('footer div span').css('display', 'none');
+    $('.text-sec2').css('opacity', '0');
 
     // スクロール位置に応じてフッターのテキストを切り替え
     if (
@@ -234,6 +198,7 @@ $(document).ready(function() {
       scrollPosition + windowHeight / 2 < section3Position
     ) {
       $('#mindText').css('display', 'block');
+      $('.text-sec2').css('opacity', '1');
     } else if (
       scrollPosition + windowHeight / 2 >= section3Position &&
       scrollPosition + windowHeight / 2 < section4Position
@@ -286,8 +251,35 @@ $(document).ready(function() {
         element.css("clip-path", "circle(30% at center)");
       } else {
         // 横幅が425未満の場合
-        element.css("clip-path", "circle(50% at center)");
+        element.css("clip-path", "circle(40% at center)");
       }
     }
   });
+});
+
+/*************************
+==== 各セクションh2のアニメーション
+*************************/
+$(document).ready(function() {
+  $(window).on('scroll', function() {
+    updateHeadingVisibility('#section3');
+    updateHeadingVisibility('#section4');
+    updateHeadingVisibility('#section5');
+    // 他のセクションも必要に応じて追加できます。
+  });
+
+  function updateHeadingVisibility(sectionId) {
+    var aboutMeSection = $(sectionId);
+    var aboutMeHeading = $(sectionId + ' h2');
+    var aboutMeHeadingPosition = aboutMeSection.offset().top;
+    var windowHeight = $(window).height();
+    var windowScrollPosition = $(window).scrollTop();
+
+    if (windowScrollPosition >= aboutMeHeadingPosition - (windowHeight / 1.5)) {
+      aboutMeHeading.css({
+        'opacity': '1',
+        'transform': 'translateY(0)'
+      });
+    }
+  }
 });
